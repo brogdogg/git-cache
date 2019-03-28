eabf7a71b0828bcff6d75c3b6f5f37bf43ba133f
Michael Brogdon <brogdogg@gmail.com>
2019-03-28 14:16:50 -0600

    Add unit test stub for git controller

---
+++ git-cache-mstest/Controllers/GitControllerUnitTest.cs
@@ -0,0 +1,26 @@
+﻿/******************************************************************************
+ * File...: GitControllerUnitTest.cs
+ * Remarks: 
+ */
+using git_cache.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace git_cache_mstest.Controllers
+{
+  /************************** GitControllerUnitTest **************************/
+  /// <summary>
+  /// Verifies the behavior of the <see cref="GitController"/> class
+  /// </summary>
+  [TestClass]
+  public class GitControllerUnitTest
+  {
+    /*======================= PUBLIC ========================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+  } /* End of Class - GitControllerUnitTest */
+}
+/* End of document - GitControllerUnitTest.cs */
