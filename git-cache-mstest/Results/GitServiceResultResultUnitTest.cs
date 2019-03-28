79cedd532e586b19292c9244395815ff302ca662
Michael Brogdon <brogdogg@gmail.com>
2019-03-28 14:15:04 -0600

    Unit test stubs for results

---
+++ git-cache-mstest/Results/GitServiceResultResultUnitTest.cs
@@ -0,0 +1,30 @@
+﻿/******************************************************************************
+ * File...: GitServiceResultResultUnitTest.cs
+ * Remarks: 
+ */
+using git_cache.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace git_cache_mstest.Results
+{
+  /************************** GitServiceResultResultUnitTest *****************/
+  /// <summary>
+  /// Verifies the behavior of the <see cref="GitServiceResultResult"/>
+  /// object.
+  /// </summary>
+  [TestClass]
+  public class GitServiceResultResultUnitTest
+  {
+    /*======================= PUBLIC ========================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+  } /* End of Class - GitServiceResultResultUnitTest */
+}
+/* End of document - GitServiceResultResultUnitTest.cs */
